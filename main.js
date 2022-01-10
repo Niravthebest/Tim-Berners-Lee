a47048711569da5915c9420447be428956c60e8d
Kiran Sanubala <83561712+Niravthebest@users.noreply.github.com>
2022-01-10 17:41:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function Change() {
+document.getElementById("Picture").img ="https://news.mit.edu/sites/default/files/images/201704/tim-berners-lee-mit-turing-award-00_1.jpg"
+}
+function Same() {
+document.getElementById("Picture").src = "https://upload.wikimedia.org/wikipedia/commons/4/4e/Sir_Tim_Berners-Lee_%28cropped%29.jpg"
+}
